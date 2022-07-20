deb9c3ceffd2104e3e84b5ad1ecd0ccf080c4454
ericz02 <ezheng0720@gmail.com>
2022-07-20 13:47:55 -0400

    Updated slider style and function

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
     //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
-    
+
   });
 });
